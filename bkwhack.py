527fda47e2151ea10e429ba4a3b257095c6ae72d
Alexander Nissen <alexander@feldbech.dk>
2023-05-19 17:14:21 +0200

    Add 7z format

--- bkwhack.py
+++ bkwhack.py
@@ -13,6 +13,12 @@ context.log_level = "warn"
 # Table of file types and byte sequences known to be at specific file offsets
 # Negative offsets are offsets from the end of the file
 CRIB_TABLE = {
+    "7z": {
+        0: b"7z\xbc\xaf\x27\x1c\x00",  # then likely \x04, but not guaranteed
+        18: b"\x00\x00",
+        26: b"\x00\x00",
+        -2: b"\x00\x00"
+    },
     "exe": {
         0: b"MZ",
         28: b"\x00" * 12
